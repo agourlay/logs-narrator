b3c3f77b06731912f0c2fcb5b6781bcdf535b68f
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2022-06-17 10:52:36 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# logs-narrator
+
+Merge logs to tell a story.
+
+## status
+
+WIP
+
+## usage
+
+`./logs-narrator $input-directory-with-logs`
---
+++ src/main.rs
@@ -0,0 +1,197 @@
+use chrono::{DateTime, FixedOffset};
+use colored::Color::*;
+use colored::Colorize;
+use colored::{Color, ColoredString};
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::{env, fs, io};
+
+fn main() -> io::Result<()> {
+    // TODO CLI args management
+    let input_dir = env::args().nth(1).unwrap();
+    let id_detection_regex = Regex::new(r"(?:newRaft, raft_id: )(\d+)").unwrap();
+
+    let log_files = load_files_in_memory(&input_dir, Some(id_detection_regex))?;
+    process_log_files(log_files);
+    Ok(())
+}
+
+fn process_log_files(log_files: Vec<LogFile>) {
+    // color index
+    let mut color_index = HashMap::new();
+    for log_file in &log_files {
+        if let Some(log_file_id) = &log_file.id {
+            color_index.insert(log_file_id.clone(), log_file.id_colored.as_ref().unwrap());
+        }
+    }
+    // hold the current offset for each files
+    let mut offsets: Vec<usize> = vec![0; log_files.len()];
+    let mut staged_lines: Vec<Option<LogEntry>> = vec![None; log_files.len()];
+    loop {
+        // rehydrate empty staged lines
+        for (index, offset) in offsets.iter().enumerate() {
+            if staged_lines[index].is_none() {
+                if let Some(line) = log_files[index].lines.get(*offset) {
+                    staged_lines[index] = Some(line.clone());
+                }
+            }
+        }
+        // exit if nothing staged
+        if staged_lines.iter().all(|l| l.is_none()) {
+            break;
+        }
+        // find value and pop it
+        let mut min_index: Option<(usize, LogEntry)> = None;
+        for (index, staged_line) in staged_lines.iter().enumerate() {
+            match staged_line {
+                None => (),
+                Some(sl) => {
+                    match &min_index {
+                        None => min_index = Some((index, sl.clone())), // first element
+                        Some((_index, existing_min)) if existing_min.timestamp > sl.timestamp => {
+                            min_index = Some((index, sl.clone())) // new min
+                        }
+                        _ => (),
+                    }
+                }
+            }
+        }
+        match min_index {
+            None => panic!("broken invariant - there must a min"),
+            Some((min_index, sl)) => {
+                // inc offset
+                offsets[min_index] += 1;
+                // remove stage value
+                staged_lines[min_index].take();
+                // print line
+                let log_file = &log_files[min_index];
+                render_log_entry(&sl, log_file, &color_index)
+            }
+        }
+    }
+}
+
+fn render_log_entry(
+    log_entry: &LogEntry,
+    log_file: &LogFile,
+    color_index: &HashMap<String, &ColoredString>,
+) {
+    if color_index.is_empty() {
+        // no additional coloring
+        println!("[{}]{}", log_file.file_name_colored, log_entry.line);
+    } else {
+        let prefix = log_file
+            .id_colored
+            .as_ref()
+            .unwrap_or(&log_file.file_name_colored);
+
+        let mut tmp = log_entry.line.clone();
+        // color ids founds
+        for (id, colored_id) in color_index {
+            tmp = tmp.replace(id, &format!("{}", &colored_id));
+        }
+
+        println!("[{}][{}]{}", log_file.file_name, prefix, tmp);
+    }
+}
+
+fn parse_date(line: &str) -> DateTime<FixedOffset> {
+    DateTime::parse_from_rfc3339(line).unwrap()
+}
+
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
+struct LogEntry {
+    timestamp: DateTime<FixedOffset>,
+    line: String,
+}
+
+struct LogFile {
+    file_name: String,
+    lines: Vec<LogEntry>,
+    id: Option<String>,
+    color: Color,
+    // cached values
+    file_name_colored: ColoredString,
+    id_colored: Option<ColoredString>,
+}
+
+// TODO Generate more than 6 colors
+const ALL_COLORS: [Color; 6] = [Red, Green, Yellow, Blue, Magenta, Cyan];
+
+/// Assume files fit in memory \o/
+fn load_files_in_memory(
+    input_dir: &str,
+    id_detection_regex: Option<Regex>,
+) -> io::Result<Vec<LogFile>> {
+    let paths = fs::read_dir(input_dir).unwrap();
+    // result
+    let mut log_files = Vec::new();
+    // keep only '.log' paths
+    let log_paths = paths.filter(|p| {
+        let path = p.as_ref().unwrap().path();
+        path.extension().and_then(|os| os.to_str()) == Some("log")
+    });
+    // analyze paths
+    for (index, path) in log_paths.enumerate() {
+        let path = path?.path();
+        let file_name = path
+            .file_name()
+            .and_then(|os| os.to_str())
+            .unwrap()
+            .to_string();
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        let content: io::Result<Vec<String>> = reader.lines().collect();
+        let content = content?;
+        // extract id
+        let id = id_detection_regex
+            .as_ref()
+            .and_then(|regex| match extract_id(&content, regex) {
+                None => {
+                    println!("Regex provided but no capture data found");
+                    None
+                }
+                Some(id) => {
+                    println!("Found id {} for file {}", id, file_name);
+                    Some(id)
+                }
+            });
+        let lines = content
+            .into_iter()
+            .map(|line| {
+                let date_str = &line[1..25]; // focus on date (will break on other format!)
+                let timestamp = parse_date(date_str);
+                let line = line.clone();
+                LogEntry { timestamp, line }
+            })
+            .collect();
+        let color = ALL_COLORS[index];
+        let file_name_colored = file_name.color(color);
+        let id_colored = id.as_ref().map(|it| it.color(color));
+        let log_file = LogFile {
+            file_name,
+            id,
+            lines,
+            color,
+            file_name_colored,
+            id_colored,
+        };
+        log_files.push(log_file);
+    }
+    Ok(log_files)
+}
+
+fn extract_id(lines: &Vec<String>, id_regex: &Regex) -> Option<String> {
+    for line in lines {
+        match id_regex.captures(line).and_then(|c| c.get(1)) {
+            None => continue,
+            Some(matched) => {
+                let id = matched.as_str().to_string();
+                return Some(id);
+            }
+        }
+    }
+    None
+}
